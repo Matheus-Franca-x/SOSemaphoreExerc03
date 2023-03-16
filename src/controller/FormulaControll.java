6f94bb1e18ef400179125e36934d8b68285f999c
matheus <matheussss6613@gmail.com>
2023-03-16 00:14:56 -0300

    Test corrida

--- src/controller/FormulaControll.java
+++ src/controller/FormulaControll.java
@@ -1,14 +1,17 @@
 package controller;
 
+import java.text.DecimalFormat;
 import java.util.concurrent.Semaphore;
 
-public class FormulaControll 
+public class FormulaControll
 {
 	
 	private int carroId = 0;
 	private int equipeId;
 	private Semaphore semaforo;
 	private Semaphore pausa;
+	private int voltas = 0;
+	public double bestTime = 0;
 	
 	public FormulaControll(int id, int equipe, Semaphore semaforo, Semaphore pausa)
 	{
@@ -50,16 +53,24 @@ public class FormulaControll
 	}
 	
 	
-
-	
 	public void iniCorrida()
 	{
-		System.out.println(this.carroId + " " + this.equipeId);
+		DecimalFormat formatacao = new DecimalFormat("0.00");
+		float tempoI = System.nanoTime();
+		float tempoF = 0;
+		while (this.voltas < 3)
+		{
+			try {
+				Thread.sleep((int) (Math.random() * 1001) + 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			tempoF = (float) ((System.nanoTime() - tempoI) / Math.pow(10, 9));
+			this.voltas++;
+			System.out.println("O carro  da quipe " + this.carroId + " numero " + this.equipeId + " correu na " + this.voltas + "° por " + formatacao.format(tempoF));
+		}
+		
+		
 	}
-	
-	
-	
-	
-	
 	
 }
